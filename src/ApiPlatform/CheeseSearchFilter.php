ced44fc81eba767ba16e4dd7b4bf84f37e2538ac
Aymeric <hello@codeparadise.xyz>
2020-10-21 09:52:30 +0200

    Custom Filter Logic for Entities

--- src/ApiPlatform/CheeseSearchFilter.php
+++ src/ApiPlatform/CheeseSearchFilter.php
@@ -10,10 +10,28 @@ class CheeseSearchFilter extends AbstractFilter
 {
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
     {
+        if ($property !== 'search') {
+            return;
+        }
+
+        $alias = $queryBuilder->getRootAliases()[0];
+        // a param name that is guaranteed unique in this query
+        $valueParameter = $queryNameGenerator->generateParameterName('search');
+        $queryBuilder->andWhere(sprintf('%s.title LIKE :%s OR %s.description LIKE :%s', $alias, $valueParameter, $alias, $valueParameter))
+            ->setParameter($valueParameter, '%'.$value.'%');
     }
 
     public function getDescription(string $resourceClass): array
     {
-        
+        return [
+            'search' => [
+                'property' => null,
+                'type' => 'string',
+                'required' => false,
+                'openapi' => [
+                    'description' => 'Search across multiple fields',
+                ],
+            ]
+        ];
     }
 }
